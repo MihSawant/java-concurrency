f2389e42706f67895aa912d9ca0b1dffcfc518e7
Mihir Sawant <mihirssawant06@gmail.com>
2022-12-29 13:04:16 +0530

    corrected back to sync as it was only soln

--- src/main/java/race_condition/Value.java
+++ src/main/java/race_condition/Value.java
@@ -1,14 +1,16 @@
 package race_condition;
 
 public class Value{
-//    private final Object key = new Object();
-    volatile int num;
+    private final Object key = new Object();
+    int num;
     public Value(int num){
         this.num = num;
     }
 
     public void incrementValue(){
-        num = num + 1;
+        synchronized (key){
+            num = num + 1;
+        }
     }
     public int getValue(){
         return num;
