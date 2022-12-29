defe9db0319b4123abaf197d25b8a89cad4267c1
Mihir Sawant <mihirssawant06@gmail.com>
2022-12-29 13:02:03 +0530

    modified num to be volatile and not use lock, not works

--- src/main/java/race_condition/Value.java
+++ src/main/java/race_condition/Value.java
@@ -1,16 +1,14 @@
 package race_condition;
 
 public class Value{
-    private final Object key = new Object();
-    int num;
+//    private final Object key = new Object();
+    volatile int num;
     public Value(int num){
         this.num = num;
     }
 
     public void incrementValue(){
-        synchronized (key){
-            num = num + 1;
-        }
+        num = num + 1;
     }
     public int getValue(){
         return num;
